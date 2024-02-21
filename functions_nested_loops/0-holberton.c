65234207f8593356acf68ae635d231a2e37da82c
mirgubad <Mirgubad@MIRGUBAD>
2024-02-21 10:22:12 -0800

    initn

---
+++ functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Mirgubad Akbarov
+ *
+ */
+
+#include "holberton.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar("\n");
+
+	return (0);
+}
