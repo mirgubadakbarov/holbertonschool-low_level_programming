ee8865ef15af4ccbb4ec2801ffbc433129474630
mirgubad <mirgubad@gmail.com>
2024-02-26 16:06:50 +0000

    print numbers 10 time

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,8 +9,8 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (j = 0; j < 10; j++)
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 15; j++)
+	for (i = 0; i <= 9; i++)
 		_putchar(j + '0');
 		
 	_putchar('\n');
