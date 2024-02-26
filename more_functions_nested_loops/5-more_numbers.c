0ae57d89adb1c1fd82e8140df7c9f52c3a436937
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-02-26 08:20:14 -0800

    Update 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,8 +8,10 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++) {
-		for (i = 0; i <= 14; i++) {
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
 			if (i > 9)
 				_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
