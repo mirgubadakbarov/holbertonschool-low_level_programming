2711a3c699e7dd8bfc6021c9fa482cb466cc62c2
mirgubad <mirgubad@gmail.com>
2024-02-26 14:50:19 +0000

    main.h added

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Mirgubad kbarov
+ * Desc: Header file containing prototypes for all functions
+ *       used in the more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
