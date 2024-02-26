b7c8bae421ee4b83d0f91c60e8217728fc48c8da
mirgubad <mirgubad@gmail.com>
2024-02-26 15:53:07 +0000

    print numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,13 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 		if (i == 2 || i == 4)
+		{
 			continue;
-	_putchar(i + '0');
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
 
 	_putchar('\n');
 }
