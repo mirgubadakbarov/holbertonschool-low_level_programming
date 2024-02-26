dfe61414b9b4195411360630928fb46d98c46442
mirgubad <mirgubad@gmail.com>
2024-02-26 15:24:55 +0000

    multiply

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
  *
- * Return: Always 0.
+ * Return: The result of the multiplication.
  */
 
-int mul(int num1, int num2)
+int mul(int num1, int num2);
 {
-	printf("%d",num1 * num2);
-
-	return (0);
+	return (num1 * num2);
 }
