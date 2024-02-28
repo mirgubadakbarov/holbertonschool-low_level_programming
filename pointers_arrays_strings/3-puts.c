982041152a04123b8f8a7eb3e6a1f0f07d9fa79c
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-02-28 09:08:32 -0800

    Update 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -11,10 +11,8 @@ void _puts(char *str)
 {
 	char *ptr = str;
 
-    // Move pointer to the end of the string
     while (*ptr)
         ptr++;
 
-    // Write string to standard output
     write(1, str, ptr - str);
 }
