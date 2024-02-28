7fff5ac3c0c24439f65982bcbdc213d066bff5fd
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-02-28 07:42:51 -0800

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,16 @@ int main(void)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
+
 		else if (num % 3 == 0)
 			printf("Fizz");
-		else if (num % 5 == 0) 
+
+		else if (num % 5 == 0)
 			printf("Buzz");
+
 		else
 			printf("%d", num);
+
 		if (num == 100)
 			continue;
 		printf(" ");
@@ -29,6 +33,4 @@ int main(void)
 	printf("\n");
 
 	return (0);
-
-
 }
