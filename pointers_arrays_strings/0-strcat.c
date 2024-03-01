dc156fe7e2357efa7f6f513df8e5edb2ffbc4d77
mirgubad <mirgubad@gmail.com>
2024-03-01 22:31:39 +0000

    strcat added

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,19 @@
 char *_strcat(char *dest, char *src)
 {
-	char *ptr = dest; 
-	
+	char *ptr = dest;
+
 	while (*ptr != '\0')
 	{
 		ptr++;
 	}
-	
-	while (*src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-	*ptr = '\0';
-	
-    return dest;
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+		*ptr = '\0';
+
+		return dest;
 }
