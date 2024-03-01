010cf10637411eec96c3dc18f2557903e521b3a1
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 02:34:10 +0400

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,10 @@
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *ptr = dest;
