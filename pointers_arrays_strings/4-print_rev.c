4082175321bb25fcc30043ceb35b564b3848a2f4
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-01 23:20:03 +0400

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
 void print_rev(char *s)
 {
     int len = 0, index = 0;
-
+    
     while (s[len] != '\0')
         len++;
-
+    
     for (index = len - 1; index >= 0; index--)
         _putchar(s[index]);
-
+    
     _putchar('\n');
 }
