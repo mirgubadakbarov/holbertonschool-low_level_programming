658e0062a126925685f8d284f94d7104cd814c03
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 02:18:19 +0400

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,13 @@
-char *_strcat(char *dest, char *src) {
-    char *ptr = dest; 
-
-    while (*ptr != '\0') {
-        ptr++;
-    }
-
-    while (*src != '\0') {
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest; 
+	
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	
+	while (*src != '\0') {
         *ptr = *src;
         ptr++;
         src++;
