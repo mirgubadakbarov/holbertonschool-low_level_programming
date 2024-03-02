63ef00395e3fee2aa9a2e50e708122693ac6379a
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 14:33:14 +0400

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,27 +4,26 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    size_t name_len, owner_len;
-    dog_t *new_dog;
-
-    new_dog = malloc(sizeof(dog_t));
+    dog_t *new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
         return NULL;
 
-    name_len = strlen(name) + 1;
-    owner_len = strlen(owner) + 1;
-
-    new_dog->name = malloc(name_len);
-    new_dog->owner = malloc(owner_len);
-    if (new_dog->name == NULL || new_dog->owner == NULL) {
-        free(new_dog->name);
-        free(new_dog->owner);
+    new_dog->name = malloc(strlen(name) + 1);
+    if (new_dog->name == NULL) {
         free(new_dog);
         return NULL;
     }
 
-    memcpy(new_dog->name, name, name_len);
-    memcpy(new_dog->owner, owner, owner_len);
+    strcpy(new_dog->name, name);
+
+    new_dog->owner = malloc(strlen(owner) + 1);
+    if (new_dog->owner == NULL) {
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
+    }
+
+    strcpy(new_dog->owner, owner);
 
     new_dog->age = age;
 
