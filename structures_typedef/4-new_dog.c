0a0ffc868f512b3e99057c3d0345a7a21e736dd3
mirgubadakbarov <mirgubad@gmail.com>
2024-03-02 10:54:24 +0000

    new dog updated

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,52 +4,59 @@
 
 char *custom_strcpy(char *dest, const char *src)
 {
-    char *p = dest;
-    
-    while ((*p++ = *src++) != '\0')
-        ;
-    return dest;
+	char *p = dest;
+
+	while ((*p++ = *src++) != '\0')
+		return (dest);
+	
 }
 
 size_t custom_strlen(const char *str)
 {
-    size_t len = 0;
-    
-    while (str[len] != '\0') {
-        len++;
-    }
-    return len;
+	size_t len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return len;
 }
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    size_t name_len, owner_len;
-    
-    dog_t *new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return NULL;
+	size_t name_len, owner_len;
 
-    name_len = custom_strlen(name);
-    owner_len = custom_strlen(owner);
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
-    new_dog->name = malloc(name_len + 1);
-    if (new_dog->name == NULL) {
-        free(new_dog);
-        return NULL;
-    }
+	if (new_dog == NULL)
+		return (NULL);
 
-    custom_strcpy(new_dog->name, name);
+	name_len = custom_strlen(name);
+	owner_len = custom_strlen(owner);
 
-    new_dog->owner = malloc(owner_len + 1);
-    if (new_dog->owner == NULL) {
-        free(new_dog->name);
-        free(new_dog);
-        return NULL;
-    }
+	new_dog->name = malloc(name_len + 1);
 
-    custom_strcpy(new_dog->owner, owner);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    new_dog->age = age;
+	custom_strcpy(new_dog->name, name);
 
-    return new_dog;
+	new_dog->owner = malloc(owner_len + 1);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	custom_strcpy(new_dog->owner, owner);
+
+	new_dog->age = age;
+
+	return new_dog;
 }
