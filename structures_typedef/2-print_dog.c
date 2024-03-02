dceebbc12b9f37278e3b065a2436fd72c05663d9
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 14:15:57 +0400

    Update 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,5 +17,5 @@ void print_dog(struct dog *d)
 
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n". d->owner ? d->owner : "(nil)");
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
