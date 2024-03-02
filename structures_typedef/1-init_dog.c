f98788dcb93eba8e4a108f5895682db153b0b1b7
mirgubadakbarov <mirgubad@gmail.com>
2024-03-02 09:45:25 +0000

    inti dog updated

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -20,7 +20,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->owner = owner;
 	
 	if (d == NULL)
-        return;
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
