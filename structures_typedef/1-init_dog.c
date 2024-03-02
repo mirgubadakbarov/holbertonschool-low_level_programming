7c3a8e866f60cbde89efd1e7b6a1112c45e652c2
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 13:39:04 +0400

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,7 +13,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-<<<<<<< HEAD
 	if (d == NULL)
 		return;
 
@@ -21,12 +20,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 
-=======
     if (d == NULL)
         return;
     
     d->name = name;
     d->age = age;
     d->owner = owner;
->>>>>>> refs/remotes/origin/main
 }
