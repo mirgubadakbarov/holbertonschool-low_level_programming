68226a09ee0ec5799546501fcfe2cac7f5dd1c3a
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 14:32:17 +0400

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,24 +2,17 @@
 #include <string.h>
 #include "dog.h"
 
-/**
- * new_dog - Creates a new dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- *
- * Description: This function creates a new dog with the given name, age, and owner.
- *              It allocates memory for the dog and copies the name and owner strings.
- *              Returns a pointer to the new dog on success, or NULL if memory allocation fails.
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog = malloc(sizeof(dog_t));
+    size_t name_len, owner_len;
+    dog_t *new_dog;
+
+    new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
         return NULL;
 
-    size_t name_len = strlen(name) + 1;
-    size_t owner_len = strlen(owner) + 1;
+    name_len = strlen(name) + 1;
+    owner_len = strlen(owner) + 1;
 
     new_dog->name = malloc(name_len);
     new_dog->owner = malloc(owner_len);
