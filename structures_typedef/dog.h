358e64fa4f3a818bc44cfa827b7c1be5c8939a59
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 13:27:03 +0400

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Defines a structure for a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure represents a dog, containing its name, age, and owner.
+ */
+
 struct dog
 {
 	char *name;
