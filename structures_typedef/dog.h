9f50d9a11444cff297f838f2529a18e1e6aa29ac
mirgubadakbarov <mirgubad@gmail.com>
2024-03-02 10:28:49 +0000

    dog.h updated

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,11 +9,12 @@
  *
  * Description:containing its name, age, and owner.
  */
+
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
