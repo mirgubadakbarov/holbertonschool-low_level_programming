0232d100d03922db73ddeb34b3a8bcd8124352cc
mirgubadakbarov <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-02 14:33:59 +0400

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,13 +2,23 @@
 #include <string.h>
 #include "dog.h"
 
+size_t custom_strlen(const char *str) {
+    const char *p = str;
+    while (*p)
+        p++;
+    return p - str;
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
         return NULL;
 
-    new_dog->name = malloc(strlen(name) + 1);
+    size_t name_len = custom_strlen(name);
+    size_t owner_len = custom_strlen(owner);
+
+    new_dog->name = malloc(name_len + 1);
     if (new_dog->name == NULL) {
         free(new_dog);
         return NULL;
@@ -16,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
     strcpy(new_dog->name, name);
 
-    new_dog->owner = malloc(strlen(owner) + 1);
+    new_dog->owner = malloc(owner_len + 1);
     if (new_dog->owner == NULL) {
         free(new_dog->name);
         free(new_dog);
