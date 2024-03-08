f316b772304bb703982792df42a0961598eb3333
Mirgubad <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-08 18:58:42 +0400

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,8 @@
  *
  * Return: A pointer to the destination buffer @dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int n) 
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	char *ptr_dest = dest;
