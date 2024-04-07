56ae9e9e4aefd52febe208c9a59df34ba00731b0
Mirgubad <154666707+mirgubadakbarov@users.noreply.github.com>
2024-04-07 22:40:06 +0400

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -6,6 +6,7 @@
  * @h: Pointer to the pointer to the head node of the list
  * @idx: Index of the list where the new node should be added
  * @n: Value to set in the new node
+ *
  * Return: The address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -13,11 +14,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *new_node, *temp;
     unsigned int count = 0;
 
-    if (h == NULL)
+    if (!h)
         return NULL;
 
     new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
+    if (!new_node)
         return NULL;
 
     new_node->n = n;
@@ -26,27 +27,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     {
         new_node->next = *h;
         new_node->prev = NULL;
-        if (*h != NULL)
+        if (*h)
             (*h)->prev = new_node;
         *h = new_node;
         return new_node;
     }
 
     temp = *h;
-    while (temp != NULL && count < idx - 1)
+    while (temp && count < idx - 1)
     {
         temp = temp->next;
         count++;
     }
 
-    if (temp == NULL)
+    if (!temp)
     {
         free(new_node);
         return NULL;
     }
 
     new_node->next = temp->next;
-    if (temp->next != NULL)
+    if (temp->next)
         temp->next->prev = new_node;
     temp->next = new_node;
     new_node->prev = temp;
