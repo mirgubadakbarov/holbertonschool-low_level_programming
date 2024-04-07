5a68ab002e705da0ce0d745343680baed362afdc
Mirgubad <154666707+mirgubadakbarov@users.noreply.github.com>
2024-04-07 22:40:36 +0400

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,11 +15,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     unsigned int count = 0;
 
     if (!h)
-        return NULL;
+        return (NULL);
 
     new_node = malloc(sizeof(dlistint_t));
     if (!new_node)
-        return NULL;
+        return (NULL);
 
     new_node->n = n;
 
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         if (*h)
             (*h)->prev = new_node;
         *h = new_node;
-        return new_node;
+        return (new_node);
     }
 
     temp = *h;
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     if (!temp)
     {
         free(new_node);
-        return NULL;
+        return (NULL);
     }
 
     new_node->next = temp->next;
@@ -52,5 +52,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     temp->next = new_node;
     new_node->prev = temp;
 
-    return new_node;
+    return (new_node);
 }
